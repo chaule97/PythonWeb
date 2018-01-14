2d5a89c14cba1703a35fdee87ef66b7ec7372ecb
Chau <lebaochau97@gmail.com>
2018-01-14 14:19:17 +0700

    Create App

---
+++ home/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase, SimpleTestCase
+
+# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
