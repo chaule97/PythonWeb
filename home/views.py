9eecb35a6b6757467fe43d2a7bec7f777cf0724e
Chau <lebaochau97@gmail.com>
2018-01-17 14:11:23 +0700

    Static and Template

--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'pages/home.html')
+def contact(request):
+    return render(request, 'pages/contact.html')
