94c130df961f22b339e1edcd4b14f57c76270ed8
Chau <lebaochau97@gmail.com>
2018-01-20 17:07:22 +0700

    Create Model

---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
