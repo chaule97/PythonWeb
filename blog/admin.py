a11d4c0a74568a38370445e5a1bfe838681283bd
Chau <lebaochau97@gmail.com>
2018-01-27 11:21:28 +0700

    Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date']
+    list_filter = ['date']
+    search_fields = ['id']
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
