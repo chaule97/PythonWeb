55e195c98ae09bd4b9cf85ed3a7c1b6932ad386a
Chau <lebaochau97@gmail.com>
2018-01-28 22:17:54 +0700

    List

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def list(request):
+    Data = {'Posts': Post.objects.all().order_by("-date")}
+    return render(request, 'blog/blog.html', Data)
