6c1f22e672e8b3146f845ea7dc8e3171ada131e9
Chau <lebaochau97@gmail.com>
2018-02-01 21:47:49 +0700

    Detail Post

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+class BlogTests(TestCase):
+    def setUp(self):
+        Post.objects.create(
+            title='myTitle',
+            body='just a Test'
+        )
+    def test_string_representation(self):
+        post = Post(title='My entry title')
+        self.assertEqual(str(post), post.title)
+    def test_post_list_view(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'just a Test')
+        self.assertTemplateUsed(response, 'blog/blog.html')
+    def test_post_detail_view(self):
+        response = self.client.get('/blog/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'myTitle')
+        self.assertTemplateUsed(response, 'blog/blog.html')
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from .models import Post
 def list(request):
     Data = {'Posts': Post.objects.all().order_by("-date")}
     return render(request, 'blog/blog.html', Data)
+def post(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'blog/post.html', {'post': post})
