8771f27cba479fbcb7dd2345e8051303008ba482
Chau <lebaochau97@gmail.com>
2018-02-11 15:29:03 +0700

    Registration Form

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import RegistrationForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request, 'pages/home.html')
@@ -7,3 +8,11 @@ def contact(request):
     return render(request, 'pages/contact.html')
 def error(request):
     return render(request, 'pages/error.html')
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
