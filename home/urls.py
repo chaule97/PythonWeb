86d27018c473f035a9e107bf7d4eab5c2a87ab15
Chau <lebaochau97@gmail.com>
2018-02-13 00:00:00 +0700

    Login Logout

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index),
     path('contact/', views.contact, name='contact'),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.login, {'template_name':'pages/login.html'}, name='login'),
+    path('logout/', auth_views.logout, {'next_page':'/'}, name='logout')
 ]
