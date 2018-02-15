a0a9d8795a785562a793da7989fa1cb8faf196e2
Chau <lebaochau97@gmail.com>
2018-02-15 23:39:42 +0700

    Generic View

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from . import views
+from django.views.generic import ListView, DetailView
+from .models import Post
 urlpatterns = [
-    path('', views.list, name='blog'),
-    path('<int:id>/', views.post, name='post'),#blog/id
+    path('', ListView.as_view(
+        queryset = Post.objects.all().order_by("-date"),
+        template_name = 'blog/blog.html',
+        context_object_name = 'Posts',
+        paginate_by = 10)
+        , name='blog'),
+    path('<int:pk>/', DetailView.as_view(
+        model = Post,
+        template_name = 'blog/post.html')
+        , name='post'),#blog/id
 ]
 #blog/java/id
 #blog/python/django/id
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from .models import Post
+
 # Create your views here.
-def list(request):
-    Data = {'Posts': Post.objects.all().order_by("-date")}
-    return render(request, 'blog/blog.html', Data)
-def post(request, id):
-    post = Post.objects.get(id=id)
-    return render(request, 'blog/post.html', {'post': post})
